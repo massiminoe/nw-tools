9023b7a33bebf0e3c0fdd03be40e5f506a7c39df
Max Ellis <ellis.m@student.unimelb.edu.au>
2022-04-25 03:03:19 +1000

    add catch all route

--- routes/baseRouter.js
+++ routes/baseRouter.js
@@ -1,16 +1,12 @@
 const express = require('express')
 
-// create our Router object 
 const baseRouter = express.Router()
 
-// import demo controller functions 
 const baseController = require('../controllers/baseController')
 
-// add a route to handle the GET request for all demo data 
 baseRouter.get('/', (req, res) => { 
-    res.send('<img src="images/beacon.png">');
+    res.render('damage_calc', {layout: false})
 });
-
 baseRouter.get('/healing-calc', (req, res) => {
     res.render('healing_calc', {layout: false})
 });
@@ -25,7 +21,8 @@ baseRouter.get('/damage_calc', (req, res) => {
     res.render('damage_calc', {layout: false})
 })
 
-//clinicianRouter.get('/:id', clinicianController.clinicianDemo);
+baseRouter.get('*', (req, res) => {
+    res.redirect('/');
+});
 
-// export the router 
 module.exports = baseRouter;
