042ca2db028849da382174e08f2661e6968b8bf6
Max Ellis <ellis.m@student.unimelb.edu.au>
2022-04-25 02:58:29 +1000

    finish rebase

--- build_tool.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, Flask, redirect, url_for, render_template, request, session, flash
-
-bp = Blueprint('build_tool', __name__)
-
-@bp.route("/build-tool", methods=["POST", "GET"])
-def build_tool():
-    return render_template("build_tool.html")
--- healing_calc.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, Flask, redirect, url_for, render_template, request, session, flash
-
-bp = Blueprint('healing_calc', __name__)
-
-@bp.route("/healing-calc", methods=["POST", "GET"])
-def healing_calc():
-    return render_template("healing_calc.html")
